3960fd656700c662720c66f5fd6aac62ca7535f2
Buguster <your.email@example.com>
2024-02-04 20:30:22 +0100

    JSX Syntax adding multiple objects

--- src/index.js
+++ src/index.js
@@ -1,48 +1,67 @@
-import React from 'react'
-import ReactDom from 'react-dom'
+import React from "react";
+import ReactDom from "react-dom";
 
 // CSS the . means that the file is in the same folder
-import './index.css'
+import "./index.css";
 
 const first_book = {
-  title :"Soul Book",
-  Author:"OLIVIA WILSON",
-  image:'https://marketplace.canva.com/EAFaQMYuZbo/1/0/1003w/canva-brown-rusty-mystery-novel-book-cover-hG1QhA7BiBU.jpg'
-}
+  title: "Soul Book",
+  Author: "OLIVIA WILSON",
+  image:
+    "https://marketplace.canva.com/EAFaQMYuZbo/1/0/1003w/canva-brown-rusty-mystery-novel-book-cover-hG1QhA7BiBU.jpg",
+};
 
-const BookList  = () => {
+const second_book = {
+  title: "Soul Book",
+  Author: "OLIVIA WILSON",
+  image: "https://i.imgur.com/g8kAD8n.jpg",
+};
+
+const third_book = {
+  title: "Soul Book",
+  Author: "OLIVIA WILSON",
+  image: "https://content.wepik.com/statics/10345869/preview-page0.jpg",
+};
+
+const BookList = () => {
   return (
-    <section className='book'>
-      <Book 
-        image={first_book.image} 
-        title={first_book.title} 
+    <section className="book">
+      <Book
+        image={first_book.image}
+        title={first_book.title}
         Author={first_book.Author}
       />
-      <Book 
-        title="random title" 
+      <Book
+        image={second_book.image}
+        title={second_book.title}
+        Author={second_book.Author}
+      />
+      <Book
+        image={third_book.image}
+        title={third_book.title}
+        Author={third_book.Author}
       />
     </section>
-  )
-}
+  );
+};
 
 const Book = (Props) => {
   return (
-  <article className='book-item'>
-    <img
-      src= {Props.image}
-      alt='coverbook'
-      style={{
-        width: '200px', // Set a fixed width
-        height: 'auto',
-        boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
-        borderRadius: '8px',
-      }}
-    />
-    <h1>{Props.title}</h1>
-    <h4>{Props.Author}</h4>
+    <article className="book-item">
+      <img
+        src={Props.image}
+        alt="coverbook"
+        style={{
+          width: "200px",
+          height: "auto",
+          boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+          borderRadius: "8px",
+        }}
+      />
+      <h1>{Props.title}</h1>
+      <h4>{Props.Author}</h4>
+    </article>
+  );
+};
 
-  </article>
-  )
-}
-
-ReactDom.render(<BookList />, document.getElementById('root1'));
+ReactDom.render(<BookList />, document.getElementById("root1"));

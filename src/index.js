c87f487b618a0883ce03f661d5828131de586748
Buguster <your.email@example.com>
2024-02-04 16:45:47 +0100

    JSX Syntax

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import ReactDom from 'react-dom'
 // CSS the . means that the file is in the same folder
 import './index.css'
 
+const title = "Soul Book";
+const Author = "OLIVIA WILSON";
+const image = 'https://marketplace.canva.com/EAFaQMYuZbo/1/0/1003w/canva-brown-rusty-mystery-novel-book-cover-hG1QhA7BiBU.jpg'
 const BookList  = () => {
   return (
     <section className='book'>
@@ -16,13 +19,13 @@ const BookList  = () => {
     </section>
   )
 }
+
+
 const Book = () => {
-  const Author = "OLIVIA WILSON";
-  const title = "Soul Book";
   return (
   <article className='book-item'>
     <img
-      src='https://marketplace.canva.com/EAFaQMYuZbo/1/0/1003w/canva-brown-rusty-mystery-novel-book-cover-hG1QhA7BiBU.jpg'
+      src= {image}
       alt='coverbook'
       style={{
         width: '200px', // Set a fixed width
@@ -37,44 +40,4 @@ const Book = () => {
   )
 }
 
-const  Greeting = () =>{
-  return (
-    <>
-    <div>
-      <Person/>
-      <h1>Hello world</h1>
-      <ul>
-        <li>  here </li>
-        <li>  you </li>
-        <li> go </li>
-       </ul>
-    </div>
-    <div>
-      <Message/>
-      <img src="" alt="" />
-    </div>
-    </>
-  );
-}
-const Person = () => <h1>soumia ouzat</h1>
-
-const Message = () => {
-  return <h1>Hello Again</h1>
-}
-
-// ReactDom.render(<Greeting />, document.getElementById('root'));
 ReactDom.render(<BookList />, document.getElementById('root1'));
-
-// we are just calling function 
-// we don't need to return html here 
-// // this is called JSX
-// function Greeting(){
-//   return <div>
-//     <h1>Hello World</h1>
-//     <h1>Hello World</h1>
-//     <h1>Hello World</h1>
-//     <h1>Hello World</h1>
-//     <h1>Hello World</h1>
-//     <h1>Hello World</h1>
-//   </div>
-// }
